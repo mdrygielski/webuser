c85abc318619c06b9bb369ef968a344b10644107
Marcin <drygielski.marcin@gmail.com>
2018-11-01 00:57:00 +0100

    aws test 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello world, AWS test<br/>
+            Hello world, AWS test 2<br/>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
