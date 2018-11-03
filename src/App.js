1e6432ca7acb3fd83d51e1b18f5f55207dcf7603
Marcin <drygielski.marcin@gmail.com>
2018-11-03 01:56:00 +0100

    AppBar v.03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
+import React,  { Component } from 'react';
 import './App.css';
+import {translate} from 'react-i18next';
+import Button from '@material-ui/core/Button'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
+
 
 class App extends Component {
   render() {
+    const {i18n } = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Żono,<br/>
-            Co będzie dzisiaj na kolację?
-          </p>
-        </header>
-        <body>
-          <img alt={"test"} src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl9R_2M4rlLj5YV1OXy52fGNMhr61NllHs4_jo8eM2lp1C7Hg5tg"}/>
-          {/*<button>Rozpocznij badanie</button>*/}
-        </body>
+          <AppBar>
+          <Toolbar>
+              {this.props.t('welcome.title', {framework: "react-i18next"})}
+
+              <FormControl>
+                  <InputLabel shrink htmlFor="language-label-placeholder">
+                      {this.props.t('welcome.language', {framework: "react-i18next"})}
+                  </InputLabel>
+                  <NativeSelect
+                      name="lang"
+                      value={i18n.language}
+                      onChange={(event) =>  i18n.changeLanguage(event.target.value)}
+                      input={<Input name="lang" id="language-label-placeholder" />}>
+                      <option value={"en"}>English</option>
+                      <option value={"pl"}>Polski</option>
+                  </NativeSelect>
+              </FormControl>
+          </Toolbar>
+      </AppBar>
+          <br/>
+          <br/>
+          <br/>
+          <img alt={"test"} src={"https://image.shutterstock.com/image-vector/good-job-stampvector-illustration-260nw-773726719.jpg"}/>
+          <button>Rozpocznij badanie</button>
+          <br/>
+          <Button>test</Button>
+          <button onClick={() => i18n.changeLanguage('en')}>en</button>
+          <button onClick={() => i18n.changeLanguage('pl')}>pl</button>
+
+
       </div>
     );
   }
 }
 
-export default App;
+export default translate('common')(App);
