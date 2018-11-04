8e4d4eeb26de1b4e117844c2086da95aabf91ef9
Marcin <drygielski.marcin@gmail.com>
2018-11-04 23:45:00 +0100

    AppBar v0.9 + language set + ipdata information

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,  { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {withNamespaces, Trans} from 'react-i18next';
 import Button from '@material-ui/core/Button'
@@ -11,100 +11,102 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            latitude: 0,
-            longitude: 0,
-            language: "",
-            country: "",
-            countryCode: "",
-            response: ""
+            location: {
+                latitude: "",
+                longitude: "",
+                isEu: "",
+                countryName: "",
+                coutryCode: "",
+                continentName: "",
+                continentCode: "",
+                flagUrl: "",
+                language: ""
+            },
+            lng: "en"
         }
-        this.getLocation()
+        this.getIpData();
+        this.getLocation();
+
     }
 
+    onLanguageUpdate = (language) => {
+        this.setState({lng: language})
+        if (language === "pl") {
+            this.props.i18n.changeLanguage("pl");
+        } else {
+            this.props.i18n.changeLanguage("en");
+        }
+    }
 
     render() {
-    const {t, i18n } = this.props;
+        const {t, i18n} = this.props;
 
-    return (
-      <div className="App">
-          <br/>
-          <br/>
-          <br/>
-          <Trans i18nKey="welcome.test">
-              testTrans
-          </Trans>
-          <WUAppBarWrapped defaultCountryCode={this.state.countryCode}/>
-          <br/>
-          <img alt={"test"} src={"https://image.shutterstock.com/image-vector/good-job-stampvector-illustration-260nw-773726719.jpg"}/>
-          <button>Rozpocznij badanie</button>
-          <br/>
-          <Button>test</Button><br/>
-          {/*{this.getLocation()}*/}
+        return (
 
-          Lat: {this.state.latitude}<br/>
-          Lng: {this.state.longitude}<br/>
-          language: {this.state.language}<br/>
-          Country: {this.state.country}
 
-      </div>
-    );
-  }
+            <div className="App">
+                <br/>
+                <br/>
+                <br/>
+                <Trans i18nKey="welcome.test">
+                    testTrans
+                </Trans>
+                <WUAppBarWrapped language={this.state.lng} onLanguageUpdate={this.onLanguageUpdate}/>
+                <Trans i18nKey="general.language"/>: {this.state.lng}
+                <br/>
+                <img alt={"test"}
+                     src={"https://image.shutterstock.com/image-vector/good-job-stampvector-illustration-260nw-773726719.jpg"}/>
+                <button>Rozpocznij badanie</button>
+                <br/>
+                <Button>test</Button><br/>
+                Lat: {this.state.location.latitude}<br/>
+                Lng: {this.state.location.longitude}<br/>
+                language: {this.state.location.language}<br/>
+                Country: {this.state.location.countryName}
 
-  getLocation() {
+            </div>
+        );
+    }
+
+    getIpData() {
+        axios.get("https://api.ipdata.co?api-key=test").then((response) => {
+                this.setState({
+                        location: {
+                            latitude: response.data.latitude,
+                            longitude: response.data.longitude,
+                            isEu: response.data.is_eu,
+                            countryName: response.data.country_name,
+                            coutryCode: response.data.country_code,
+                            continentName: response.data.continent_name,
+                            continentCode: response.data.continent_code,
+                            flagUrl: response.data.flag,
+                            language: response.data.languages[0].name
+                        }
+                    }
+                )
+                if (response.data.country_code === "PL") {
+                    this.setState({lng: 'pl'})
+                    this.props.i18n.changeLanguage("pl");
+
+                }
+                return true;
+            }
+        ).catch((error) => {
+            return error.message;
+        })
+    }
+
+    getLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
-
                 this.setState({
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude
                 })
-                axios.get('https://api.geonames.org/countryCodeJSON', {
-                    params: {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
-                        username: "mdrygielski"
-                    }
-                }).then((response) => {
-                    console.log("response");
-                    if (response.status === 200) {
-                        this.setState({
-                            language: response.data.languages,
-                            country: response.data.countryName,
-                            countryCode: response.data.countryCode
-                        })
-                        if (response.data.countryCode === "PL") {
-                            this.props.i18n.changeLanguage("pl");
-                        } else {
-                            this.props.i18n.changeLanguage("en");
-                        }
-                    }else {
-                        this.setState({
-                            language: "en",
-                            country: "United Kingdom",
-                            countryCode: "GB"
-                        })
-                        this.props.i18n.changeLanguage("en");
-                    }
-
-                }).catch((error) =>{
-                    console.log("ERROR");
-                    this.setState({
-                        language: "en",
-                        country: "United Kingdom",
-                        countryCode: "GB"
-                    })
-                    this.props.i18n.changeLanguage("en");
-                })
-
             });
         }
-        this.setState({
-            language: "en",
-            country: "United Kingdom",
-            countryCode: "GB"
-        })
-        this.props.i18n.changeLanguage("en");
     }
+
 }
 
 
--- src/WUAppBar.js
+++ src/WUAppBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Trans } from 'react-i18next';
+import {Trans} from 'react-i18next';
 import Typography from '@material-ui/core/Typography';
-
-import ReactFlagsSelect from 'react-flags-select';
-import 'react-flags-select/css/react-flags-select.css';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Input from "@material-ui/core/Input";
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
 
 const styles = {
     root: {
@@ -15,50 +16,50 @@ const styles = {
     },
     grow: {
         flexGrow: 1,
-    },
-    menuButton: {
-        marginLeft: -12,
-        marginRight: 20,
-    },
+    }
 };
 
-function WUAppBar(props) {
-    const {classes} = props;
 
-    function onSelectFlag(countryCode)  {
-        if (countryCode === "PL") {
-            props.i18n.changeLanguage("pl");
-        } else {
-            props.i18n.changeLanguage("en");
-        }
+class WUAppBar extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
     }
-    if (props.defaultCountryCode !== "") {
-        console.log(props.defaultCountryCode);
-    return (
-        <div>
-            Country Code: {props.defaultCountryCode}
-            <AppBar>
-                <Toolbar>
-                    <Typography variant="h6" color="inherit" className={classes.grow}>
-                        <Trans i18nKey="welcome.title"/>
-                    </Typography>
-                    <ReactFlagsSelect
-                        onSelect={onSelectFlag}
-                        defaultCountry={props.defaultCountryCode === "PL" ? "PL" : "GB"}
-                        countries={["GB", "PL"]} customLabels={{"GB": "en", "PL":"pl"}}/>
-                </Toolbar>
-            </AppBar>
 
-        </div>
-    );}
-    else {
-        return "";
+    render() {
+        return (
+            <div className={this.props.classes.root}>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit" className={this.props.classes.grow}>
+                            <Trans i18nKey="welcome.title"/>
+                        </Typography>
+                        <FormControl >
+                            <InputLabel shrink htmlFor="language-label-placeholder">
+                                <Trans i18nKey="general.language" />
+                            </InputLabel>
+                            <NativeSelect
+                                value={this.props.language}
+                                onChange={(e) => {
+                                    this.props.onLanguageUpdate(e.target.value)
+                                }}
+                                input={<Input name="age" id="language-label-placeholder" />}
+                            >
+                                <option value="pl">PL</option>
+                                <option value="en">EN</option>
+                            </NativeSelect>
+                        </FormControl >
+                    </Toolbar>
+                </AppBar>
+            </div>
+
+        )
     }
 
 }
 
-// WUAppBar.propTypes = {
-//     classes: PropTypes.object.isRequired,
-// };
+WUAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(WUAppBar);
